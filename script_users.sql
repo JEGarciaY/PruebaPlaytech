8229c039dd74a382b33b046bcc1bbde9a5e97605
JEGarciaY <gjohan184@gmail.com>
2025-10-04 19:27:27 -0500

    Create script_users.sql

---
+++ script_users.sql
@@ -0,0 +1,7 @@
+-- INSERT ADMIN
+INSERT INTO users (password, role, user_name)
+VALUES ('admin123', 'Administrador', 'Admin');
+
+-- INSERT CAJERO
+INSERT INTO users (password, role, user_name)
+VALUES ('cajero123', 'Cajero', 'Cajero');
